d103fc5dea06d23f14b61ffeefa4fb809bfb98a8
SOVEREIGN <isimemeni@gmail.com>
2019-11-17 21:53:39 -0500

    added jquery to survey html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 
-var newPerson = {
+var friends = {
     name:"Ahmed",
     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores:[
@@ -16,4 +16,4 @@ var newPerson = {
       ]
   }
 
-  module.exports = newPerson;
+  module.exports = friends;
