158ca19ba4db49539bd6da07b2dc4b6aecdc413d
SOVEREIGN <isimemeni@gmail.com>
2019-11-18 22:23:21 -0500

    added option values to survey html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,13 @@
+var path = require('path');
+
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friend = require("../data/friends.js");
-
-
+var friendData = require("../data/friends.js");
+// console.log(friendData)
 
 // ===============================================================================
 // ROUTING
@@ -16,15 +17,18 @@ module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends/", function(req, res) {
-    res.json(friend);
+    res.json(friendData);
   });
 
   app.post("/api/friends/", function(req, res) {
-    
+    surveyResults = req.body.scores
+    console.log(surveyResults)
+
+
   });
+
 
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path")
+var bodyParser = require("body-parser")
 
 // Sets up the Express App
 // =============================================================
